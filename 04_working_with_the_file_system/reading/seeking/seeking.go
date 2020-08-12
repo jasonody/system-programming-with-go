96d9ace43d03783089990f0a2cfe6cb8ae64146c
Jason Ody <jasonody@users.noreply.github.com>
2020-08-11 20:50:32 -0700

    seeking

---
+++ 04_working_with_the_file_system/reading/seeking/seeking.go
@@ -0,0 +1,33 @@
+// go run seeking.go ../sample.txt
+// Should print "Read data: Some"
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please specify a file path.")
+		return
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Cannot open:", err)
+	}
+
+	file.Seek(9, io.SeekStart)
+	data := make([]byte, 4)
+	_, err = file.Read(data)
+
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	fmt.Println("Read data:", string(data))
+}
