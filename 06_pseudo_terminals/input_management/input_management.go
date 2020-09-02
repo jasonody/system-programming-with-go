fa24102ed93e853b8e0289adfae2092c3fc4195f
Jason Ody <jasonody@users.noreply.github.com>
2020-09-01 20:35:36 -0700

    improved unknown command handling

--- 06_pseudo_terminals/input_management/input_management.go
+++ 06_pseudo_terminals/input_management/input_management.go
@@ -10,7 +10,58 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/agnivade/levenshtein"
 )
+
+func exit(w io.Writer, args []string) bool {
+	fmt.Fprintf(w, "Goodbye! :)\n")
+	return true
+}
+
+func help(w io.Writer, args []string) bool {
+	fmt.Fprintln(w, "Available commands:")
+	for _, cmd := range cmds {
+		fmt.Fprintf(w, "  - %-15s %s\n", cmd.Name, cmd.Help)
+	}
+	return false
+}
+
+func shuffle(w io.Writer, args []string) bool {
+	rand.Shuffle(len(args), func(i, j int) {
+		args[i], args[j] = args[j], args[i]
+	})
+	for i := range args {
+		if i > 0 {
+			fmt.Fprint(w, " ") // add a space to seperate words
+		}
+		fmt.Fprintf(w, "%s", args[i])
+	}
+	fmt.Fprintln(w)
+	return false
+}
+
+func print(w io.Writer, args []string) bool {
+	if len(args) != 1 {
+		fmt.Fprintln(w, "Please specify one file.")
+		return false
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		fmt.Fprintf(w, "Cannot open %s: %s\n", args[0], err)
+		return false
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(w, f); err != nil {
+		fmt.Fprintf(w, "Cannot print %s: %s\n", args[0], err)
+		return false
+	}
+
+	fmt.Fprintln(w)
+	return false
+}
 
 type cmd struct {
 	Name   string                                // the command name
@@ -82,7 +133,7 @@ func main() {
 		}
 
 		if idx == -1 {
-			fmt.Fprintf(w, "%q not found. Use `help` for available commands\n", args[0])
+			commandNotFound(w, args[0])
 			continue
 		}
 
@@ -93,51 +144,26 @@ func main() {
 	}
 }
 
-func exit(w io.Writer, args []string) bool {
-	fmt.Fprintf(w, "Goodbye! :)\n")
-	return true
-}
+func commandNotFound(w io.Writer, cmd string) {
+	var suggestions []string
 
-func help(w io.Writer, args []string) bool {
-	fmt.Fprintln(w, "Available commands:")
-	for _, cmd := range cmds {
-		fmt.Fprintf(w, "  - %-15s %s\n", cmd.Name, cmd.Help)
-	}
-	return false
-}
-
-func shuffle(w io.Writer, args []string) bool {
-	rand.Shuffle(len(args), func(i, j int) {
-		args[i], args[j] = args[j], args[i]
-	})
-	for i := range args {
-		if i > 0 {
-			fmt.Fprint(w, " ") // add a space to seperate words
+	for _, c := range cmds {
+		if levenshtein.ComputeDistance(c.Name, cmd) < 3 {
+			suggestions = append(suggestions, c.Name)
 		}
-		fmt.Fprintf(w, "%s", args[i])
+	}
+
+	fmt.Fprintf(w, "Command %q not found. ", cmd)
+	if len(suggestions) == 0 {
+		fmt.Fprint(w, "Use `help` for available commands.\n")
+		return
+	}
+	fmt.Fprint(w, "Maybe you meant: ")
+	for i, suggestion := range suggestions {
+		if i > 0 {
+			fmt.Fprint(w, ", ")
+		}
+		fmt.Fprintf(w, "%s", suggestion)
 	}
 	fmt.Fprintln(w)
-	return false
-}
-
-func print(w io.Writer, args []string) bool {
-	if len(args) != 1 {
-		fmt.Fprintln(w, "Please specify one file.")
-		return false
-	}
-
-	f, err := os.Open(args[0])
-	if err != nil {
-		fmt.Fprintf(w, "Cannot open %s: %s\n", args[0], err)
-		return false
-	}
-	defer f.Close()
-
-	if _, err := io.Copy(w, f); err != nil {
-		fmt.Fprintf(w, "Cannot print %s: %s\n", args[0], err)
-		return false
-	}
-
-	fmt.Fprintln(w)
-	return false
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jasonody/system-programming-with-go
 
 go 1.14
+
+require github.com/agnivade/levenshtein v1.1.0
